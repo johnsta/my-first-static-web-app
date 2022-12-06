22aa9bb8d43189d6fb31068b64768a709797d608
John Stallo <johnsta@microsoft.com>
2022-12-06 08:37:16 +0000

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import Slider from "@madzadev/image-slider";
+import "@madzadev/image-slider/dist/index.css";
 
-function App() {
-  const [data, setData] = useState('');
+const App = () => {
+  const images = [
+    { url: "1.jpeg" },
+    { url: "2.jpeg" },
+    { url: "3.jpeg" },
+    { url: "4.jpeg" }
+  ];
 
-  useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
-
-  return <div>Hello World! {data}</div>;
+  return <Slider autoPlay={false} imageList={images} width={800} height={800} />;
 }
 
-export default App;
+export default App
