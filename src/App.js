c6ed7352d2c21c5a62d6b14870a2134e410d9d4e
John Stallo <johnsta@microsoft.com>
2022-12-09 07:46:51 +0000

    revert images

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import "@madzadev/image-slider/dist/index.css";
 const App = () => {
   const images = [
     { url: "1.jpeg" },
-    { url: "5.jpg" },
-    { url: "6.jpg" },
+    { url: "2.jpeg" },
+    { url: "3.jpeg" },
+    { url: "4.jpeg" },
   ];
 
   return <Slider autoPlay={false} imageList={images} width={800} height={800} />;
